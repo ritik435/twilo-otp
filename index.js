a69f1e5a15c276cef748e53cbebd908ca3d186a5
Ritik <ritik12022001@gmail.com>
2022-08-29 10:40:25 +0530

    verification and add to mongoDB is done

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const twilio = require('twilio');
+const mongoose = require('mongoose');
+const User = require("./model/User.js")
+// const alert = require("alert-node");
 
 const app = express();
-const port = 5000;
+const PORT = 5000;
 
+// const accountSid = "AC7004b4b93ef7ad7f9fb5cfae399ef789";
+// const authToken = "03f0a9468a76f30f227eea870ea771b3";
+// const serviceId = "MG0a0852f29c0c57cf7751819d3d652f43";
 const accountSid = "AC0a193273b45d2ba265b0b06e602bd3b3";
 const authToken = "7af94407c5c3b0bb4b06d5852f800d33";
-const serviceId = "MGbc680112d878628eb775534a068d8509"
+const serviceId = "VA2a3abf1d1eb62dfdd87861f23aeba4bb"
+
 const client = require('twilio')(accountSid, authToken);
 
 
@@ -16,30 +22,71 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
+app.get("/send-message", (req, res) => {
+    client.messages
+        .create({
+            body: 'Hello from Node',
+            to: '+919817326281', // Text this number
+            from: '+19786788831', // From a valid Twilio number
+        })
+        .then((message) => console.log(message.sid));
+})
+
+
+app.post('/store-in', (req, res) => {
+    User.findOne({ phone: req.body.phone, email: req.body.email }, function (err, user) {
+        if (err) {
+            console.log("error in finding user in sign up", err); return;
+        }
+        if (!user) {
+
+
+            User.create(req.body, (err, user) => {
+                if (err) {
+                    console.log("error in creating user in sign up", err);
+                    return res.status(404).json({ err });
+                }
+                console.log("User created");
+                return res.status(200).json({ user: "created" });
+
+            });
+        }
+        else {
+            console.log("already exist");
+            return res.status(200).json({ user: "already-exist" });
+        }
+    })
+
+
+})
+
+
 app.post('/send-verification', (req, res) => {
     const { phone } = req.body;
 
     client.verify.v2
         .services(serviceId)
         .verifications
-        .create({ to: `+91${phone}`, channel: 'sms' })
+        .create({ to: "+91" + phone, channel: 'sms' })
         .then(verification => {
+            // console.log(verification.sid);
             return res.status(200).json({ verification })
         }).catch(err => {
-            console.log("err in sending", err);
+            // console.log("err in sending", err);
             return res.status(400).json({ err });
         })
 
 })
 
 app.post('/verify', (req, res) => {
-    const { phone, otp } = req.body;
+    const { phone, code } = req.body;
 
     client.verify.v2
         .services(serviceId)
         .verificationChecks
-        .create({ to: '+91' + phone, code: otp })
+        .create({ to: '+91' + phone, code })
         .then(verification_check => {
+            console.log(verification_check);
             return res.status(200).json({ verification_check })
         }).catch(err => {
             return res.status(400).json({ err });
@@ -49,6 +96,10 @@ app.post('/verify', (req, res) => {
 
 
 
-app.listen(port, () => {
-    console.log(`App is listening to port`);
-})
+const CONNECTION_URL = "mongodb+srv://ritik:sa5AnRXfIF59Dchu@cluster0.5wspxnl.mongodb.net/test";
+
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`http://localhost:${PORT}/`)))
+    .catch((error) => console.log(error.message));
+
--- public/loginForm.js
+++ public/loginForm.js
@@ -1,22 +1,90 @@
+var phone1;
+
+const message = document.getElementById("btn");
 const loginForm = document.getElementById("login-form");
+const codeInput = document.getElementById("code-input");
 const baseUrl = "http://localhost:5000/";
 var phone;
-loginForm.addEventListener("submit", (e) => {
+var email;
+let isOtpDelivered = false;
+
+
+
+loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     phone = parseInt(document.getElementById("phone").value)
     if (isNaN(phone)) {
         alert('Invalid Mobile Number');
     } else {
         //something
-        sendVerification(phone);
+        if (isOtpDelivered) {
+            const code = codeInput.value;
+            const verifyRes = await verifyOTP(phone, code);
+            setResponse(verifyRes.status);
+            return;
+        }
+
+        const response = await sendVerification(phone);
+        if (response.status === 'pending') {
+            codeInput.classList.remove("hidden");
+            isOtpDelivered = true;
+        }
     }
 
 })
 
 async function sendVerification(phone) {
-    const res = await axios.post(baseUrl + 'send-verification', {
-        phone,
-    });
+    const res = await axios.post(baseUrl + 'send-verification', { phone });
     console.log(res);
+    if (res.status === 200) {
+        return res.data.verification;
+    } else {
+        return res.data;
+    }
 }
 
+
+async function verifyOTP(phone, code) {
+    const res = await axios.post(baseUrl + `verify`, { phone, code });
+    // console.log(res);
+    if (res.status === 200) {
+        return res.data.verification_check;
+    } else {
+        return res.data;
+    }
+}
+async function VerifyMongo(phone, email) {
+    phone = parseInt(document.getElementById("phone").value);
+    email = document.getElementById("email").value;
+    const res = await axios.post(baseUrl + 'store-in', { email, phone });
+    return res.data.user;
+}
+
+async function setResponse(status) {
+    if (status === "approved") {
+        const mongoRes = await VerifyMongo(phone, email);
+        if (mongoRes === 'created') {
+            alert('User created');
+        } else if (mongoRes === 'already-exist') {
+            alert('User already exist');
+        }
+    }
+    else if (status === "pending") alert('Invalid OTP');
+}
+
+
+
+
+
+
+
+message.addEventListener("click", (e) => {
+    e.preventDefault();
+    phone1 = parseInt(document.getElementById("phone1").value)
+    sendMessage(phone);
+
+})
+async function sendMessage(phone) {
+    const res = await axios.get(baseUrl + 'send-message');
+    console.log(res);
+}
